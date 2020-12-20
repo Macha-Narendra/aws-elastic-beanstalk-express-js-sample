6492873e1d6fc5a5aaf3eed5a50c1908d3089c02
Narendra Babu <babu.narendra229@gmail.com>
2020-12-20 14:21:39 +0530

    final changes updated by Narendra

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!, Welcome to AWS Learning Narendra Babu'));
+app.get('/', (req, res) => res.send('Very GooD Narendra Babu, FInally you have deployed your FIRST DEVOPS into AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
